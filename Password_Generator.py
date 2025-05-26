c643a277169d6d0c2e8e15398a29d711a9d72493
Sanath Kandimalla <sanath@mac.local.ftr>
2025-05-26 10:26:41 -0700

    Create and complete password generator program

---
+++ Password_Generator.py
@@ -0,0 +1,33 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password? "))
+nr_symbols = int(input(f"How many symbols would you like? "))
+nr_numbers = int(input(f"How many numbers would you like? "))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+pool = []
+
+for num1 in range(1, nr_letters+1):
+    pool.append(random.choice(letters))
+for num2 in range(1, nr_symbols+1):
+    pool.append(random.choice(symbols))
+for num2 in range(1, nr_numbers+1):
+    pool.append(random.choice(numbers))
+
+# print(pool)
+
+password = ""
+random.shuffle(pool)
+for str in pool:
+    password+= str
+
+print(password)
