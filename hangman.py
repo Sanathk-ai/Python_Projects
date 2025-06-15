4f7013ffdd4a425a8df06aeee4cc7e710b097c74
Sanath Kandimalla <sanath@mac.local.ftr>
2025-06-14 18:01:56 -0700

    hangman complete

--- hangman.py
+++ hangman.py
@@ -54,10 +54,30 @@ diagrams= ['''  +---+
 
 rand_word = random.choice(words)
 lives = 6
+
+letter_index = []
+guessed = []
 print(rand_word)
-while lives > 0:
-    print("Word to Guess: "+ f"{len(rand_word)*'_'}")
+print("Word to Guess: "+ f"{len(rand_word)*'_'}")
+def dashes():
+    ind = 0
+    dash = ""
+    for char in rand_word:
+        if char == guessed_letter:
+            letter_index.append(ind)
+        ind += 1
+    for index in range(len(rand_word)):
+        if index in letter_index:
+            dash += rand_word[index]
+        else:
+            dash += "_"
+    print("Word to Guess: " + f"{dash}")
+
+while lives > 0 and (len(letter_index)!=len(rand_word)):
+
     guessed_letter = input("Guess a letter: ")
+    if guessed_letter in guessed:
+        print("You've already guessed that letter!")
 
     if guessed_letter not in rand_word:
         lives -= 1
@@ -65,11 +85,13 @@ while lives > 0:
         print(diagrams[diag_count])
         diag_count += 1
         print(f"****************************{lives}/6 LIVES LEFT****************************")
-    else:
-        letter_index = rand_word.find(guessed_letter)
+    elif guessed_letter not in guessed:
+        guessed.append(guessed_letter)
+        dashes()
 
-        print(letter_index)
-if(lives == 0):
+if lives == 0:
     print(f"***********************IT WAS {rand_word}! YOU LOSE**********************")
+else:
+    print(f'You Guessed it right!! The word is "{rand_word}"')
 
 
